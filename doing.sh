b0d883b34262bb1cccfe7552dd01e8246696ae02
jaimito aleman <organizedfellow@gmail.com>
2016-01-26 18:02:05 -0700

    happy with current status of scipt

--- doing.sh
+++ doing.sh
@@ -1,60 +1,38 @@
 #!/bin/sh
 
-# sleep 1
-
 cd ~/WhatAreYouDoing
 
-# SOURCES --------------------------------------------------------------------------
-# http://www.shellhacks.com/en/HowTo-Check-If-a-File-Exists
-# http://www.cyberciti.biz/faq/linux-unix-formatting-dates-for-display/
-# http://www.cyberciti.biz/faq/bash-prepend-text-lines-to-file/
-# http://www.thegeekstuff.com/2009/06/15-practical-crontab-examples/
-# http://stackoverflow.com/questions/15826180/why-my-shell-script-doesnt-open-the-terminal-though-it-gives-output-log-absolu
-# http://cheesehead-techblog.blogspot.com/2008/08/opening-terminal-window-using-cron.html
-# http://askubuntu.com/questions/624701/where-are-cron-logs-stored-on-ubuntu-14-04
-# ----------------------------------------------------------------------------------
-
-
-# VARIABLE = 2016-01-20_Wed
-DATE=$(date +"%Y-%m-%d_%a")
-
-# FILE=$DATE
-DAILYFILE="$DATE.txt"
-# echo $FILE
+DATE=$(date +"%Y-%m-%d_%a")    # VARIABLE = 2016-01-20_Wed
+DAILYFILE="$DATE.txt"          # FILE=$DATE
 PASTHOUR="PASTHOUR.txt"
+TIMESTAMP=$(date +"%Y-%m-%d_%a_%T")
 
-
-# IF FILE EXISTS AND HAS A SIZE GREATER THAN ZERO
-if [ -s "$DAILYFILE" ];
-    then cat > $DAILYFILE;
-    echo "What are you doing?";
-    echo "Previously you entered: "
-    cat $PASTHOUR;
-    rm $PASTHOUR;
-    echo "\nWhat are you doing NOW?";
+if [ -e "$DAILYFILE" ];
+then
+    if [ -s "$PASTHOUR" ];
+    then
+        echo "What are you doing?"
+        echo "\nPreviously you entered:\n-----------------------"
+        cat $PASTHOUR;
+        echo "\n\nWhat are you doing NOW?";
+        rm $PASTHOUR;
+    else
+        echo "\nPrevious entry was blank."
+        echo "What are you doing NOW?";
+    fi
     read DOING
-    TIMESTAMP=$(date +"%Y-%m-%d_%a_%T")
-    # echo -e "" $TIMESTAMP\n\t$DOING > $FILE
     printf "$DOING" >> $PASTHOUR
     printf "\n$TIMESTAMP\t\t$DOING" >> $DAILYFILE
-# fi
+    sleep 1;
+    echo "\n\nThank you!";
 else
-
-# Check if the file '2016-01-20_Wed.txt' exists and perform some action if it is true :
-# if test -f "$DAILYFILE";
-    # then echo "The File Exists";
-    # then echo "What are you doing?";
-    echo "What are you doing?";
-    echo "Previously you entered: "
-    cat $PASTHOUR;
-    rm $PASTHOUR;
-    echo "\nWhat are you doing NOW?";
+    echo "GOOD MORNING!";
+    echo "\nWhat are you doing?";
     read DOING
-    TIMESTAMP=$(date +"%Y-%m-%d_%a_%T")
-    # echo -e "" $TIMESTAMP\n\t$DOING > $FILE
     printf "$DOING" >> $PASTHOUR
     printf "\n$TIMESTAMP\t\t$DOING" >> $DAILYFILE
+    sleep 1;
+    echo "Thank you!";
 fi
-
 
 sleep 1 && exit
--- what.sh
+++ what.sh
@@ -6,3 +6,5 @@
 # BUT IF I CALL IT FROM ANOTHER SCRIPT, IT WORKS!
 # SO THATS WHAT I DID
 DISPLAY=:0.0 /usr/bin/xfce4-terminal -x /home/jaimito/WhatAreYouDoing/doing.sh
+
+# DISPLAY=:0.0 /usr/bin/xfce4-terminal -x ~/WhatAreYouDoing/doing.sh
